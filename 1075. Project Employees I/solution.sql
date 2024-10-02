20b4e997ef0fcbbfdb9d8cfbb6581ca19d94c19e
ARYAN SONI <arusoni477@gmail.com>
2024-10-02 13:53:59 +0530

    added few files

---
+++ 1075. Project Employees I/solution.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT project.project_id, ROUND(AVG(Employee.experience_years), 2) AS average_years
+FROM project 
+LEFT JOIN employee ON project.employee_id = employee.employee_id 
+GROUP BY project.project_id 
