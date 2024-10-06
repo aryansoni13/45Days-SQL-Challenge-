1788a327892c3d8e7fd95323fe480d42dab8ebcc
ARYAN SONI <arusoni477@gmail.com>
2024-10-06 22:01:14 +0530

    added few files

---
+++ 619. Biggest Single Number/solution.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT
+    MAX(num) AS num
+FROM
+    MyNumbers
+WHERE
+    num NOT IN (
+        SELECT
+            num
+        FROM
+            MyNumbers
+        GROUP BY
+            num
+        HAVING
+            COUNT(num) > 1
+    );
