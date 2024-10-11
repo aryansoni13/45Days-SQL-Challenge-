9c08c0f308650e356372f37747b406b14cbd298a
ARYAN SONI <arusoni477@gmail.com>
2024-10-11 19:24:22 +0530

    Added few files

---
+++ 185. Department Top Three Salaries/solution.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+WITH RankedSalaries AS (
+    SELECT e.id, e.name AS Employee, e.salary, e.departmentId, d.name AS Department,
+           DENSE_RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS salary_rank
+    FROM Employee e
+    JOIN Department d ON e.departmentId = d.id
+)
+SELECT Department, Employee, salary AS Salary
+FROM RankedSalaries
+WHERE salary_rank <= 3;
